fda5dadb7617f7cc5f7c0e446338ea01875d1445
karnaxus <karnaxus@yourihosting.com>
2026-06-30 03:26:31 -0600

    Add files via upload

---
+++ data/towers.lua
@@ -0,0 +1,210 @@
+--[[
+    +-----------------------------------------------------+
+    | =================================================== |
+    | X TOWER DEFENSE: REFORGED                           |
+    | =================================================== |
+    | @author Karnaxus#11289                              |
+    | =================================================== |
+    | Towers                                              |
+    | =================================================== |
+    | All the towers for XTD.                             |
+    | =================================================== |
+    +-----------------------------------------------------+
+]]
+
+--[[
+    All the towers for XTD: Reforged.
+]]
+XTD.Data.Towers = {
+    [FourCC("o003")] = {
+        name = "Nightwatch Tower",
+        gold = 75,
+        tier = 1
+    },
+
+    [FourCC("o000")] = {
+        name = "Blackwatch Tower",
+        gold = 175,
+        tier = 2
+    },
+
+    [FourCC("o002")] = {
+        name = "Grimwatch Tower",
+        gold = 350,
+        tier = 3
+    },
+
+    [FourCC("o001")] = {
+        name = "Deadeye Tower",
+        gold = 650,
+        tier = 4
+    },
+
+    [FourCC("n00I")] = {
+        name = "Frostguard Tower",
+        gold = 110,
+        tier = 1
+    },
+
+    [FourCC("n00K")] = {
+        name = "Icewatch Tower",
+        gold = 235,
+        tier = 2
+    },
+
+    [FourCC("n00S")] = {
+        name = "Winterhold Tower",
+        gold = 425,
+        tier = 3
+    },
+
+    [FourCC("n00D")] = {
+        name = "Blizzardspire Tower",
+        gold = 800,
+        tier = 4
+    },
+
+    [FourCC("n00G")] = {
+        name = "Ember Tower",
+        gold = 85,
+        tier = 1
+    },
+
+    [FourCC("n00H")] = {
+        name = "Flameguard Tower",
+        gold = 190,
+        tier = 2
+    },
+
+    [FourCC("n00L")] = {
+        name = "Inferno Tower",
+        gold = 380,
+        tier = 3
+    },
+
+    [FourCC("n00J")] = {
+        name = "Hellspire Tower",
+        gold = 710,
+        tier = 4
+    },
+
+    [FourCC("n00R")] = {
+        name = "Venom Tower",
+        gold = 75,
+        tier = 1
+    },
+
+    [FourCC("n00Q")] = {
+        name = "Toxin Tower",
+        gold = 175,
+        tier = 2
+    },
+
+    [FourCC("n00M")] = {
+        name = "Plaque Tower",
+        gold = 350,
+        tier = 3
+    },
+
+    [FourCC("n00C")] = {
+        name = "Blightspire Tower",
+        gold = 650,
+        tier = 4
+    },
+
+    [FourCC("n00A")] = {
+        name = "Runic Tower",
+        gold = 90,
+        tier = 1
+    },
+
+    [FourCC("n008")] = {
+        name = "Arcane Tower",
+        gold = 190,
+        tier = 2
+    },
+
+    [FourCC("n00B")] = {
+        name = "Spellweaver Tower",
+        gold = 380,
+        tier = 3
+    },
+
+    [FourCC("n009")] = {
+        name = "Astral Spire",
+        gold = 700,
+        tier = 4
+    },
+
+    [FourCC("n004")] = {
+        name = "Spark Tower",
+        gold = 85,
+        tier = 1
+    },
+
+    [FourCC("n005")] = {
+        name = "Stormwatch Tower",
+        gold = 185,
+        tier = 2
+    },
+
+    [FourCC("n006")] = {
+        name = "Tempest Tower",
+        gold = 370,
+        tier = 3
+    },
+
+    [FourCC("n007")] = {
+        name = "Thunderspire Tower",
+        gold = 680,
+        tier = 4
+    },
+
+    [FourCC("n00N")] = {
+        name = "Shade Tower",
+        gold = 90,
+        tier = 1
+    },
+
+    [FourCC("n00F")] = {
+        name = "Dreadwatch Tower",
+        gold = 195,
+        tier = 2
+    },
+
+    [FourCC("n00P")] = {
+        name = "Shadowkeep Tower",
+        gold = 395,
+        tier = 3
+    },
+
+    [FourCC("n00E")] = {
+        name = "Doomspire Tower",
+        gold = 700,
+        tier = 4
+    },
+
+    [FourCC("h000")] = {
+        name = "Bombard Tower",
+        gold = 85,
+        tier = 1
+    },
+
+    [FourCC("h003")] = {
+        name = "War Cannon",
+        gold = 200,
+        tier = 2
+    },
+
+    [FourCC("h002")] = {
+        name = "Siege Cannon",
+        gold = 375,
+        tier = 3
+    },
+
+    [FourCC("h001")] = {
+        name = "Cataclysm Tower",
+        gold = 710,
+        tier = 4
+    }
+}
